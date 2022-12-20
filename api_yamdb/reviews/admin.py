d9986bbbe6c514a2fc7b315a79c5c1b6b4ab7be4
Олег Суворов <admin@olegtsss.ru>
2022-12-20 23:55:42 +0600

    Приступил к моделям. Сделал User, Category, Genry

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import User, Category, Genry
+
+
+class CummonAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'slug'
+    )
+    list_editable = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class CategoryAdmin(CummonAdmin):
+    ...
+
+
+class GenryAdmin(CummonAdmin):
+    ...
+
+
+admin.site.register(User)
+
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Genry, GenryAdmin)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+LENGTH_TEXT = 15
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+
+
+class CreatedModel(models.Model):
+    """Абстрактная модель. Добавляет разные атрибуты."""
+
+    name = models.CharField(
+        'Наименование',
+        max_length=256,
+        help_text='Введите наименование'
+    )
+    slug = models.SlugField(
+        'Уникальный slug',
+        unique=True,
+        max_length=50,
+        help_text='Введите уникальный идентификатор'
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Category(CreatedModel):
+    """Категории (типы) произведений"""
+
+    class Meta:
+        verbose_name = 'категорию'
+        verbose_name_plural = 'Категории на портале'
+
+
+class Genry(CreatedModel):
+    """Категории жанров"""
+
+    class Meta:
+        verbose_name = 'жанры'
+        verbose_name_plural = 'Жанры на портале'
