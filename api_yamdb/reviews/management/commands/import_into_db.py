98a1e3509ef1c9169f37bdbc0874b2c05537fb4b
Олег Суворов <admin@olegtsss.ru>
2022-12-23 22:37:03 +0600

    Импорт заработал для первой таблицы

--- api_yamdb/reviews/management/commands/import_into_db.py
+++ api_yamdb/reviews/management/commands/import_into_db.py
@@ -9,22 +9,30 @@ HELP_MESSAGE = 'Импорт данных из static/data/*.csv'
 START_MESSAGE = 'Начинаем импорт...'
 STOP_MESSAGE = 'Иморт закончен...'
 IMPORT_ERROR = 'Что-то пошло не так: {error}.'
+IMPORT_MESSAGE = 'Обрабатывается набор данных: {data}'
 
 
 def import_into_db_from_csv():
     """Импорт информации из csv файлов в базу данных."""
-    print('Hello!')
+
+    # files = []
+    # for file in files:
     with open('api_yamdb/static/data/category.csv', encoding='utf-8') as file:
         reader = csv.reader(file)
         # Пропускаем заголовки
         next(reader)
         Category.objects.all().delete()
         for row in reader:
-            print(row)
+            print(IMPORT_MESSAGE.format(data=row))
             Category.objects.get_or_create(name=row[1], slug=row[2])
 
 
 class Command(BaseCommand):
+    """
+    Класс для работы managment комманды.
+    python api_yamdb/manage.py import_into_db
+    """
+
     help = HELP_MESSAGE
 
     def handle(self, *args, **options):
